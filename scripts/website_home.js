eb214a26cfc58a336745c69fa5a9214024f5a1d4
JonathanStarks <jonstarks@byui.edu>
2024-06-30 09:17:14 -0600

    I think I got all the pages to have something on them

--- scripts/website_home.js
+++ scripts/website_home.js
@@ -2,8 +2,13 @@ document.getElementById("clicks").onclick = change_everything;
 
 function change_everything()
 {
-    document.body.style.fontFamily = 'Courier New';
+    
     document.body.style.color = '#66FF00';
     
     return false;
+}
+
+function expand_menu()
+{
+
 }
