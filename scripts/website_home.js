d9da48680b459a4d2be9c2768b56128b7429d548
JonathanStarks <jonstarks@byui.edu>
2024-07-06 12:46:25 -0600

    Got the menu button to work on the mobile site

--- scripts/website_home.js
+++ scripts/website_home.js
@@ -4,11 +4,10 @@ function change_everything()
     document.body.style.color = '#00FF00';
     document.body.style.backgroundImage = "none";
     document.body.style.backgroundColor = "#FF00FF";
-    
     return false;
 }
-
-function expand_menu()
-{
-
-}
+    
+document.getElementById("menu_box").addEventListener("click", function(){
+    var menuBar = document.getElementById("menu_bar");
+    menuBar.classList.toggle("show");
+});
