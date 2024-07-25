b95684af597230cb86cf862062edea3cae86f7a3
JonathanStarks <jonstarks@byui.edu>
2024-07-25 14:36:27 -0600

    Added a thing that grows a flower as you click the pile of dirt

--- scripts/website_home.js
+++ scripts/website_home.js
@@ -7,7 +7,34 @@ function change_everything()
     return false;
 }
     
-document.getElementById("menu_box").addEventListener("click", function(){
+document.getElementById("menu_box").addEventListener("click", function()
+{
     var menuBar = document.getElementById("menu_bar");
     menuBar.classList.toggle("show");
 });
+
+click_count = 0;
+function grow_box()
+{
+    document.getElementById("grow").addEventListener("click", function()
+    {
+        click_count += 1;
+        document.getElementById("clicks").innerHTML = click_count;
+        if (click_count >= 40)
+        {
+            document.getElementById("grow").src = "styles/images/flower_sprout.png";
+        }
+        else if (click_count >= 30)
+        {
+            document.getElementById("grow").src = "styles/images/bud_sprout.png";
+        }
+        else if (click_count >= 20)
+        {
+            document.getElementById("grow").src = "styles/images/bigger_sprout.png";
+        }
+        else if (click_count >= 10)
+        {
+            document.getElementById("grow").src = "styles/images/sprout.png";
+        }
+    },{once: true});
+}
